0abcf2474f100b2d44602564752e1d8d5471a534
artisan <73726591+JaeHongDev@users.noreply.github.com>
2023-01-13 15:35:09 +0900

    Time: 70 ms (80.83%), Space: 41.8 MB (94.20%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,22 +3,19 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
-    const stack = []; 
-
     const compare = (target,original) => {
         if(original === "(") return target === ")";
         if(original === "[") return target === "]";
         if(original === "{") return target === "}";
         return false;
     }
-    const maxLength = s.length;
-    for(let i = 0 ; i < maxLength;i++ ){
-        if(stack.size !== 0 && compare(s[i], stack[stack.length-1])){
-            stack.pop();
-            continue; 
-        } 
-        stack.push(s[i]);
-    }
-    return stack.length === 0 
+    return s.split("").reduce((pre,cal) => {
+        if(pre.length === 0) return [cal];
+        if(compare(cal, pre[pre.length-1])){
+            pre.pop();
+            return pre; 
+        }
+        pre.push(cal);
+        return pre;
+    }, []).length === 0;
 };
