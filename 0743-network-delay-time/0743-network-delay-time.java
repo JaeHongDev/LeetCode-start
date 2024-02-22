290651a3a17f25fe093eb5a06d40585ffd1a7914
artisan <73726591+JaeHongDev@users.noreply.github.com>
2024-02-22 16:35:40 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,38 +1,36 @@
 import java.util.*;
-
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-
         Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
 
-        for(int[] time: times){
+        for(var time: times){
             graph.putIfAbsent(time[0], new HashMap<>());
             graph.get(time[0]).put(time[1], time[2]);
         }
 
-        Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Map.Entry.comparingByValue());
+        var pq = new PriorityQueue<Map.Entry<Integer, Integer>>(Map.Entry.comparingByValue());
 
         pq.add(new AbstractMap.SimpleEntry<>(k, 0));
-
+        
         Map<Integer, Integer> dist = new HashMap<>();
-        var max = 0;
+
         while(!pq.isEmpty()){
             var cur = pq.poll();
             var u = cur.getKey();
             var distU = cur.getValue();
 
-            if(dist.containsKey(u)) continue;
 
-            dist.put(u, distU);
-            max = Math.max(distU, max);
-            if(!graph.containsKey(u)) continue;
-            for(var v: graph.get(u).entrySet()){
-                var alt = distU + v.getValue();
-                pq.add(new AbstractMap.SimpleEntry<>(v.getKey(), alt));
+            if(dist.continsKey(u)) continue;
+            dist.put(new AbstractMap.SimpleEntry<>(u, distU));
+
+            if(!graph.containsKey(u)) continue; 
+
+            for(var node: graph.getKey(u).entrySet()){
+                pq.poll(new AbstractMap.SimpleEntry<>(node.getKey(), node.getValue() + distU));
             }
         }
 
-        if(dist.size() == n) return max;
+        if(dist.size() == n) return Collections.max(dist.getValues());
         return -1;
-    }
+   }
 }
