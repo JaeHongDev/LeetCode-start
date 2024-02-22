866b7739155f026b0c9087f92ba5aade60f88a02
artisan <73726591+JaeHongDev@users.noreply.github.com>
2024-02-22 15:47:42 +0900

    Time: 50 ms (7.06%), Space: 47.7 MB (74.15%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+
+        for(int[] time: times){
+            graph.putIfAbsent(time[0], new HashMap<>());
+            graph.get(time[0]).put(time[1], time[2]);
+        }
+
+        Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Map.Entry.comparingByValue());
+
+        pq.add(new AbstractMap.SimpleEntry<>(k, 0));
+
+        Map<Integer, Integer> dist = new HashMap<>();
+
+        while(!pq.isEmpty()){
+            var cur = pq.poll();
+            var u = cur.getKey();
+            var distU = cur.getValue();
+
+            if(dist.containsKey(u)) continue;
+
+            dist.put(u, distU);
+            if(!graph.containsKey(u)) continue;
+            for(var v: graph.get(u).entrySet()){
+                var alt = distU + v.getValue();
+                pq.add(new AbstractMap.SimpleEntry<>(v.getKey(), alt));
+            }
+        }
+
+        if(dist.size() == n) return Collections.max(dist.values());
+        return -1;
+    }
+}
