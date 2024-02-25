601c716239301811096c1e4b211cf42a8b169f5c
artisan <73726591+JaeHongDev@users.noreply.github.com>
2024-02-25 16:20:24 +0900

    Time: 3 ms (76.44%), Space: 42.5 MB (94.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
         var left = 0; 
         var right = s.length() - 1;
 
         while(left < right) {
 
-            var l = s.charAt(left);
-            var r = s.charAt(right);
+            var l = Character.toLowerCase(s.charAt(left));
+            var r = Character.toLowerCase(s.charAt(right));
 
             if(!(Character.isAlphabetic(l) || Character.isDigit(l))) left++;
             else if(!(Character.isAlphabetic(r) || Character.isDigit(r))) right--;
