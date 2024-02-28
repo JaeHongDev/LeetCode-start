6c489e010492716e6df8d0b0ee7dc90cd2afb3d5
artisan <73726591+JaeHongDev@users.noreply.github.com>
2024-02-28 12:41:23 +0900

    Time: 0 ms (100%), Space: 40.4 MB (30.8%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,9 @@
 class Solution {
+    int[] cache = new int[31];
     public int fib(int n) {
         if(n == 0) return 0;
         if(n == 1) return 1;
-        return fib(n-1) + fib(n -2); 
+        if(cache[n] != 0) return cache[n];
+        return cache[n] = fib(n-1) + fib(n -2); 
     }
 }
