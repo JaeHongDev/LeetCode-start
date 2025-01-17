d75851e50c34eb2820aa69751ba933b126c44b95
artisan <73726591+JaeHongDev@users.noreply.github.com>
2025-01-17 10:12:07 +0900

    Time: 31 ms (57.7%), Space: 51.3 MB (89.77%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // -1 -1 0 1 2 4
-        List<List<Integer>> answer = new ArrayList<>();
 
+        List<List<Integer>> answer = new ArrayList<>();
+        
         Arrays.sort(nums);
 
         for(int i = 0; i < nums.length - 2; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            var left = i + 1;
-            var right = nums.length - 1;
-
-            var num = nums[i];
-
-            while(left < right){
-                var sum = num + nums[left] + nums[right];
-
-
-                if(sum == 0) {
-                    answer.add(List.of(num, nums[left], nums[right]));
-
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;
-                    left++;
-                    right--;
-                }else if(sum > 0)  right--;
-                else  left++;
+            var ll =  i + 1; 
+            var rl = nums.length - 1;
+            while(ll < rl){
+                var sum = nums[i] + nums[ll] + nums[rl];
+                if(sum < 0) ll += 1 ;
+                else if(sum > 0) rl -= 1; 
+                if(sum == 0){
+                    answer.add(List.of(nums[i], nums[ll], nums[rl]));
+                    while(ll < rl && nums[ll] == nums[ll + 1]) ll++;
+                    while(ll < rl && nums[rl] == nums[rl - 1]) rl--;
+                    ll++;
+                    rl--;
+                }
             }
         }
         return answer;
