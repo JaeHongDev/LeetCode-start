8f4500b9ce2c389ccb07494e7edc9306de1f8c09
artisan <73726591+JaeHongDev@users.noreply.github.com>
2025-02-03 14:03:16 +0900

    Time: 0 ms (100%), Space: 47 MB (8.15%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,23 @@
 class Solution {
     public int trap(int[] height) {
+        var size = height.length;
+        var sum = 0;
         var ll = 0;
-        var rl = height.length - 1; 
+        var rl = size - 1;
+        var lmax = height[0];
+        var rmax = height[rl];
 
-        var volume = 0;
-
-        var lm = height[ll];
-        var rm = height[rl];
-
-        while(ll < rl) {
-            lm = Math.max(height[ll], lm);
-            rm = Math.max(height[rl], rm);
-
-            if(height[ll] <= height[rl]) volume += lm - height[ll++];
-            else volume += rm - height[rl--];
-        } 
-
-        return volume;
-    }
+        // 왼쪽과 오른쪽에서 출발해 가장 큰 기둥을 찾아가는 과정
+        while(ll <= rl) {
+            if(lmax <= rmax) {
+                lmax = Math.max(lmax, height[ll]);
+                sum += lmax - height[ll++];
+            }else {
+                rmax = Math.max(rmax, height[rl ]);
+                sum += rmax - height[rl--];
+            }
+        }
+        
+        return sum;
+    } 
 }
